1ae8efc2c3102ca06c7f88238776c8b09f0a3ab7
abdalafo98 <abdalafo.abubaker@gmail.com>
2021-12-06 11:01:08 +0200

    vuex str

--- store/index.js
+++ store/index.js
@@ -1,24 +1,12 @@
 import axios from "axios";
-
-export const state = () => ({
-  nameData: "hhhhh",
-});
-
-export const getters = {
-  nameData(state) {
-    return state.nameData;
-  },
-};
-export const mutations = {
-  addUsers(state, payload) {
-    state.nameData = payload.type;
-    console.log("payload.type", payload.type);
-  },
+import login from "./modules/login";
+import Vuex from "vuex";
+const createStore = () => {
+  return new Vuex.Store({
+    modules: {
+      login,
+    },
+  });
 };
 
-export const actions = {
-  async LOGIN({ commit }, payload) {
-    const result = await axios.post("http://localhost:5000/login", payload);
-    console.log(result);
-  },
-};
+export default createStore;
---
+++ store/modules/login.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const login = {
+  namespace: true,
+  actions: {
+    async LOGIN({ commit }, payload) {
+      const result = await axios.post("http://localhost:5000/login", payload);
+      console.log(result);
+    },
+  },
+  mutations: {},
+  getters: {},
+};
+export default login;
